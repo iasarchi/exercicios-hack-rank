520508743c02e44b4c7cfca28faee79ca16cda34
iasarchi <iasmimcardoso11@gmail.com>
2022-03-31 16:08:30 -0300

    :)

---
+++ src/exercicioshackerank/output/model/Exercicio3.java
@@ -0,0 +1,14 @@
+package exercicioshackerank.output.model;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n +"x"+i+ "="+i * n);
+        }
+    }
+}
