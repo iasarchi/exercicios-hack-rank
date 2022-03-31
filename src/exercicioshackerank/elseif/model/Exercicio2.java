7b80b147538d08c907d09448506d675c621b5bb8
iasarchi <iasmimcardoso11@gmail.com>
2022-03-31 11:31:45 -0300

    :)

--- src/exercicioshackerank/elseif/model/Exercicio2.java
+++ src/exercicioshackerank/elseif/model/Exercicio2.java
@@ -1,5 +1,17 @@
 package exercicioshackerank.elseif.model;
 
-public class Exercicio2 {
+import java.util.Scanner;
 
+public class Exercicio2 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int i = scanner.nextInt();
+    double d = scanner.nextDouble();
+    scanner.nextLine();
+    String s = scanner.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+}
 }
