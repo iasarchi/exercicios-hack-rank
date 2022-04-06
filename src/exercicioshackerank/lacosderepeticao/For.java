31dbcd4bd63faa766ba6c7ca85bc59bf29d37c43
iasarchi <iasmimcardoso11@gmail.com>
2022-04-06 15:48:36 -0300

    :)

---
+++ src/exercicioshackerank/lacosderepeticao/For.java
@@ -0,0 +1,32 @@
+/*
+We use the integers a, b, and n to create the following series:
+
+(a+2°*b),(a+2°*b+2¹*b),....
+
+You are given  queries in the form of a, b, and n. For each query, print the series corresponding
+to the given a, b, and n values as a single line of  space-separated integers.
+
+ */
+
+package exercicioshackerank.lacosderepeticao;
+
+import java.util.Scanner;
+
+public class For {
+    Scanner scanner= new Scanner(System.in);
+    int t= scanner.nextInt();{
+        for (int i = 0; i < t; i++) {
+            int a=scanner.nextInt();
+            int b=scanner.nextInt();
+            int n=scanner.nextInt();
+            for (int j = 0; j < n ; j++) {
+            int funcao = a;
+
+                funcao += (Math.pow(2,j)*b);
+                System.out.print(funcao+" ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
---
+++ src/exercicioshackerank/lacosderepeticao/While.java
@@ -0,0 +1,12 @@
+package exercicioshackerank.lacosderepeticao;
+
+public class While {
+    public static void main(String[] args) {
+
+    int count = 0;
+    while(count < 20){
+          System.out.println(count);
+          count=count+1;
+      }
+   }
+}
